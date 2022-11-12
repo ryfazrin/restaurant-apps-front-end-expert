a829915961634ff587d6baa3d091594b7f48e84b
ryfazrin <riansera515@gmail.com>
2022-11-12 14:20:45 +0700

    updated: mobile style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,15 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/fonts.css';
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("drawer");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
