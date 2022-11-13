29034023b17422be80ffbeec977d18bfd50d5c15
ryfazrin <riansera515@gmail.com>
2022-11-13 13:48:53 +0700

    created: dynamic filling stars

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,17 @@
+// Scripts
 import 'regenerator-runtime'; /* for async await transpile */
+import { fillingStar } from './utils/fill-star';
+import data from '../DATA.json';
+// Styles
 import '../styles/fonts.css';
 import '../styles/main.css';
 import '../styles/responsive.css';
-import data from '../DATA.json';
 
 document.addEventListener("DOMContentLoaded", function () {
   const restaurantList = document.querySelector(".content");
   restaurantList.innerHTML = "";
  
-  data.restaurants.forEach((restaurant) => {
+  data.restaurants.forEach((restaurant, index) => {
     const restaurantItem = document.createElement("article");
     restaurantItem.classList.add("headline");
     restaurantItem.innerHTML = `
@@ -17,12 +20,19 @@ document.addEventListener("DOMContentLoaded", function () {
       <figcaption>${restaurant.city}</figcaption>
     </figure>
     <div class="headline__content">
-      <p>Rating: 4.5</p>
+      <p>
+        Rating: ${restaurant.rating} 
+        <span class="stars-outer">
+          <span class="stars-inner"></span>
+        </span>
+      </p>
       <h1 class="headline__title">${restaurant.name}</h1>
       <p class="headline__description">${restaurant.description}</p>
     </div>
     `;
     restaurantList.appendChild(restaurantItem);
+
+    fillingStar(index, restaurant.rating);
   });
 });
 
@@ -30,6 +40,11 @@ const hamburgerButtonElement = document.querySelector('#hamburger');
 const drawerElement = document.querySelector('#drawer');
  
 hamburgerButtonElement.addEventListener('click', event => {
+  drawerElement.classList.toggle('open');
+  event.stopPropagation();
+});
+
+hamburgerButtonElement.addEventListener('keypress', event => {
   drawerElement.classList.toggle('open');
   event.stopPropagation();
 });
---
+++ src/scripts/utils/fill-star.js
@@ -0,0 +1,7 @@
+export function fillingStar(index, rating) {
+  const starTotal = 5;
+
+  const starPercentage = (rating / starTotal) * 100;
+  const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+  document.querySelectorAll('.stars-inner')[index].style.width = starPercentageRounded;
+}
