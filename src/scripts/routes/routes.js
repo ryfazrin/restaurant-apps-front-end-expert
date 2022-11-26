00a1f66aea18f2de433bf9bec10ac47427c1088f
ryfazrin <riansera515@gmail.com>
2022-11-26 21:09:48 +0700

    created: routes system

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,12 @@
+import Restaurants from '../views/pages/restaurants';
+import Detail from '../views/pages/detail';
+// import Like from '../views/pages/like';
+
+const routes = {
+  '/': Restaurants,
+  '/restaurants': Restaurants,
+  '/detail/:id': Detail,
+  // '/like': Like,
+};
+
+export default routes;
