7b1e464bc50402a5dc4ba682ed3698526b10ff3b
ryfazrin <riansera515@gmail.com>
2022-11-26 18:53:13 +0700

    updated: config base image url

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/' + 'images/medium/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: 'MealDadu-V1',
   DATABASE_NAME: 'meal-dadu-restaurant-database',
