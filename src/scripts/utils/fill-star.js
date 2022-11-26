36b4811d0ed7cc2aa599cac120cf5b4bdf9f1edd
ryfazrin <riansera515@gmail.com>
2022-11-26 18:55:01 +0700

    updated: fill-star

--- src/scripts/utils/fill-star.js
+++ src/scripts/utils/fill-star.js
@@ -1,7 +1,9 @@
-export function fillingStar(index, rating) {
+function fillingStar(index, rating) {
   const starTotal = 5;
 
   const starPercentage = (rating / starTotal) * 100;
   const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
   document.querySelectorAll('.stars-inner')[index].style.width = starPercentageRounded;
 }
+
+export default fillingStar;
