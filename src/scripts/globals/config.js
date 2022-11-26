147361e7c4d189574a87dffc938824652a580836
ryfazrin <riansera515@gmail.com>
2022-11-26 16:45:04 +0700

    created: config

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/' + 'images/medium/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MealDadu-V1',
+  DATABASE_NAME: 'meal-dadu-restaurant-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant',
+};
+
+export default CONFIG;
