5eadf2750037d6fcfb44170f8ce78edc4cddaa44
ryfazrin <riansera515@gmail.com>
2022-11-27 12:15:51 +0700

    created: add like button

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import TheRestaurantDbSource from '../../data/therestaurantdb-source';
 import UrlParser from '../../routes/url-parser';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 import * as template from '../templates/template-creator';
 
 const Restaurants = {
   async render() {
     return `
       <section id="restaurant" class="restaurant"></section>
+      <div id="likeButtonContainer"></div>
     `;
   },
 
@@ -13,6 +15,10 @@ const Restaurants = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await TheRestaurantDbSource.detailRestaurant(url.id);
     const restaurantContainer = document.querySelector('#restaurant');
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant,
+    });
 
     restaurantContainer.innerHTML = template.restaurantDetail(restaurant);
 
