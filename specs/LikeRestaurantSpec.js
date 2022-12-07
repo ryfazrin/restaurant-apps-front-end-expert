305f05236b0e4991e3afd074e18bffef0517ef30
ryfazrin <riansera515@gmail.com>
2022-12-07 09:01:54 +0700

    created: first spec Like restaurant

---
+++ specs/LikeRestaurantSpec.js
@@ -0,0 +1,23 @@
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+
+describe('Liking a Restaurant', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(() => {
+    addLikeButtonContainer();
+  });
+
+  it('should show the like button when the movie has not been liked before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this restaurant"]'))
+      .toBeTruthy();
+  });
+});
