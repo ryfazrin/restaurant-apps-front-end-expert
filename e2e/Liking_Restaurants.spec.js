7ad873bc5435bdf6e377c9ff3a381575d3c1910a
ryfazrin <riansera515@gmail.com>
2022-12-08 00:05:06 +0700

    added: new scenario liking one restaurant

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
@@ -6,4 +8,27 @@ Before(({ I }) => {
 
 Scenario('showing empty liked restaurants', ({ I }) => {
   I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found');
+});
+
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found');
+
+  I.amOnPage('/');
+
+  I.waitForElement('.headline__title a', 10);
+
+  I.seeElement('.headline__title a');
+
+  const firstRestaurant = locate('.headline__title a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.headline');
+  const likedRestaurantTitle = await I.grabTextFrom('.headline__title');
+
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 });
