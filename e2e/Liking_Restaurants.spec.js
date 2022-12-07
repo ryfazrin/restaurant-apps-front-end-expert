ce5d7fad8e3baa149a6ca6fd0655405e51a94019
ryfazrin <riansera515@gmail.com>
2022-12-08 00:00:35 +0700

    created: first scenario liking restaurants showing empty liked restaurants

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,5 +1,9 @@
 Feature('Liking Restaurants');
 
-Scenario('test something', ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
 
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found');
 });
