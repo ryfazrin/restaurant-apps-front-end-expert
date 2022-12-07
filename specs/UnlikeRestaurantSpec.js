2d0d3f953399c5b28beee70ffa710d4cae727f9a
ryfazrin <riansera515@gmail.com>
2022-12-07 09:33:01 +0700

    added: test in unlike restaurant spec should display unlike widget when the restaurant has been liked

--- specs/UnlikeRestaurantSpec.js
+++ specs/UnlikeRestaurantSpec.js
@@ -1,1 +1,25 @@
-describe('Unliking a Restaurant', () => {});
+import FavoriteRestaurantDb from '../src/scripts/data/favorite-restaurant-idb';
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+
+describe('Unliking a Restaurant', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestaurantDb.putRestaurant({ id: 1 });
+  });
+
+  it('should display unlike widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this restaurant"]'))
+      .toBeTruthy();
+  });
+});
